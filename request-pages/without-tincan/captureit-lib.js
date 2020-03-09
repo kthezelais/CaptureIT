bf5848f56a85eae8d504aff607e28aac633dc7f7
Killian THEZELAIS <killian.thezelais@imt-atlantique.net>
2020-03-09 22:11:29 +0100

    Add getStatements() to captureit-lib.js

--- request-pages/without-tincan/captureit-lib.js
+++ request-pages/without-tincan/captureit-lib.js
@@ -65,3 +65,42 @@ function getAboutLRS(endPoint) {
         }
     });
 }
+
+//http://127.0.0.1:8000/trax/ws/xapi/statement
+
+/*
+ *  Get a statement by its ID, and the LRS endPoint.
+ *  parameters user and password are required to (auth created in LRS DB.
+ *
+ *  return : object of the $.ajax() request.
+ *      SUCCESS : <result_var>.status == 200
+ *      ERROR : <result_var>.status == 400
+ * 
+ *  Check with status attribut to see if the request works or not.
+ */
+function getStatements(endPoint, user, password) {
+    auth = btoa(user + ':' + password);
+
+    return $.ajax({
+        beforeSend: function(req) {
+            req.setRequestHeader("Content-Type", "application/json");
+            req.setRequestHeader("Authorization", "Basic " + auth);
+            req.setRequestHeader("X-Experience-API-Version", "1.0.0");
+            console.log("Headers sucessfully initialized");
+        },
+
+        async: false,
+        url: endPoint + "/trax/ws/xapi/statements",
+        type: "GET",
+        dataType: "json",
+
+        success: function(resultat, statut, erreur) {
+            console.log("RESULTAT : \n" + JSON.stringify(resultat) + "\n\nSTATUT : " + statut + "\n\nERREUR : " + JSON.stringify(erreur));
+        },
+
+        error: function(resultat, statut, erreur) {
+            console.log("RESULTAT : \n" + JSON.stringify(resultat) + "\n\nSTATUT : " + statut + "\n\nERREUR : " + JSON.stringify(erreur));
+        }
+
+    });
+}

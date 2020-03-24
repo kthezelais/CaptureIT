4a1d0d1f93fc5314e761f6f166dca4f3aea87e88
Killian THEZELAIS <killian.thezelais@imt-atlantique.net>
2020-03-24 14:58:56 +0100

    Add authentication feature

--- backend/login.php
+++ backend/login.php
@@ -1,7 +1,6 @@
 <?php
 
 header("Content-Type: application/json; charset=UTF-8");
-
 // Takes raw data from the request
 $json = file_get_contents('php://input');
 
@@ -9,8 +8,6 @@ $json = file_get_contents('php://input');
 $data = json_decode($json);
 
 $url = "http://10.0.2.15:8000/trax/ws/xapi/statements?statementId=xxxxx-xxx-xxxxx";
-//$url = "http://10.0.2.15:8000/trax/ws/xapi/about";
-
 
 $options = array(
     'http' => array(
@@ -28,16 +25,16 @@ $result = file_get_contents($url, false, $context);
 
 // Afficher l'erreur code : var_dump($http_response_header);
 
-
 if($http_response_header[0] == "HTTP/1.0 401 Unauthorized") {
+  header("HTTP/1.0 401 Unauthorized");
   echo "Login ou mot de passe incorrect.";
 } else if($http_response_header[0] == "HTTP/1.0 400 Bad Request") {
-  echo "Vous êtes connectés";
-} else if($http_response_header[0] == "HTTP/1.0 404 Not Found") {
-  echo "Une erreur s'est produite. Vérifier que votre serveur TRAX-LRS fonctionne correctement.";
-} else if($http_response_header == null) {
-  echo "Une erreur s'est produite. Vérifier que votre serveur TRAX-LRS fonctionne correctement.";
+  //session_start();
+  header("HTTP/1.0 200 OK");
+  //$_SESSION['content'] = $data->content;
+  echo "Connexion réussi !";
 } else {
-  echo "Une erreur s'est produite. Réessayez ou revenez plus tard.";
+  header("HTTP/1.0 500 Internal Server Error");
+  echo "Une erreur s'est produite. Vérifier que votre serveur TRAX-LRS ainsi que votre backend sont bien congigurés.";
 }
 ?>
